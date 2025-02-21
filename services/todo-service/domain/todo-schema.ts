df411570f234395e45f406bb2793d1f65718135d
ali <ali@gmail.com>
2025-02-21 12:37:13 +0330

    fix: todo schema

--- services/todo-service/domain/todo-schema.ts
+++ services/todo-service/domain/todo-schema.ts
@@ -1,10 +1,12 @@
 import { Static, Type } from '@sinclair/typebox';
 
 export const todoSchema = Type.Object({
-  id: Type.Optional(Type.Integer()),
+  id: Type.Integer(),
   userId: Type.Integer(),
   title: Type.String(),
   status: Type.String(),
 });
 
-export type addTodoDTO = Static<typeof todoSchema>;
+export const addTodoSchema = Type.Omit(todoSchema, ['id']);
+
+export type addTodoDTO = Static<typeof addTodoSchema>;
