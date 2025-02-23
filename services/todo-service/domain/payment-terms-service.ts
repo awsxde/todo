2cf840a95501e9685e70c0ebc758169dcaa58ee4
ali <ali@gmail.com>
2025-02-23 21:47:34 +0330

    fix: remove payment terms service

--- services/todo-service/domain/payment-terms-service.ts
+++
@@ -1,7 +0,0 @@
-// ️️️✅ Best Practice: Use services for scoped and specific pieces of business logic
-function determinePaymentTerms(requestedTerms: number, userId: number) {
-  // In real-world app, more logic and even integrations will come here
-  return requestedTerms + userId || 30;
-}
-
-export default { determinePaymentTerms };
