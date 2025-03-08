826e2a13bff2310903fc5b5cfc98861f79ed2b34
ali <ali@gmail.com>
2025-03-08 10:56:49 +0330

    fix: routes imports

--- services/todo-service/entry-points/api/routes.ts
+++ services/todo-service/entry-points/api/routes.ts
@@ -1,7 +1,10 @@
 import { logger } from '@practica/logger';
 import express from 'express';
 import util from 'util';
-import * as newTodoUseCase from '../../domain/new-todo-use-case';
+import { addTodo } from '../../domain/use-case/add-user-use-case';
+import { deleteTodo } from '../../domain/use-case/delete-user-use-case';
+import { getTodo } from '../../domain/use-case/get-user-use-case';
+import { updateTodo } from '../../domain/use-case/update-user-use-case';
 
 export default function defineRoutes(expressApp: express.Application) {
   const router = express.Router();
@@ -12,7 +15,7 @@ export default function defineRoutes(expressApp: express.Application) {
         `Todo API was called to add new Todo ${util.inspect(req.body)}`
       );
       // ✅ Best Practice: Using the 3-tier architecture, routes/controller are kept thin, logic is encapsulated in a dedicated domain folder
-      const addTodoResponse = await newTodoUseCase.addTodo(req.body);
+      const addTodoResponse = await addTodo(req.body);
       return res.json(addTodoResponse);
     } catch (error) {
       next(error);
@@ -25,7 +28,7 @@ export default function defineRoutes(expressApp: express.Application) {
       logger.info(
         `Todo API was called to update a Todo ${util.inspect(req.body)}`
       );
-      const updateTodoResponse = await newTodoUseCase.updateTodo(req.body);
+      const updateTodoResponse = await updateTodo(req.body);
       return res.json(updateTodoResponse);
     } catch (error) {
       next(error);
@@ -37,7 +40,7 @@ export default function defineRoutes(expressApp: express.Application) {
   router.get('/:id', async (req, res, next) => {
     try {
       logger.info(`Todo API was called to get todo by id ${req.params.id}`);
-      const result = await newTodoUseCase.getTodo(parseInt(req.params.id, 10));
+      const result = await getTodo(parseInt(req.params.id, 10));
 
       if (!result) {
         res.status(404).end();
@@ -53,7 +56,7 @@ export default function defineRoutes(expressApp: express.Application) {
   // delete todo by id
   router.delete('/:id', async (req, res) => {
     logger.info(`Todo API was called to delete todo ${req.params.id}`);
-    await newTodoUseCase.deleteTodo(parseInt(req.params.id, 10));
+    await deleteTodo(parseInt(req.params.id, 10));
     res.status(204).end();
   });
 
